c6999e53501d8f60a9f3f8b577a88703cdef6d5c
Manikanta <konugantimanikantareddy@gmail.com>
2024-04-19 09:57:04 +0530

    Added Text

---
+++ mysql.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+TIMESTAMP=$( date +%F-%H-%M-%S) #Executing command in shell script and  taking output in variable
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1) # $0 : Script Name  (Ex: echo 11-functions.sh | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log #(In temp directoy lo name hypen timestamp.log (File))
+
+echo "Script started executing at: $TIMESTAMP"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2....$R FAILURE $N"
+        exit 400
+    else
+        echo -e "$2....$G SUCCESS $N"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "You must be a ROOT USER"
+    exit 1 #Manually exit if error comes
+else
+    echo "You are the ROOT USER"
+fi
+
+dnf install mysql-server -y &>>$LOGFILE
+VALIDATE $? "Installing My-SQL Server...."
+
+systemctl enable mysqld &>>LOGFILE
+VALIDATE $? "Enabling MY-SQL Server....."
+
+systemctl start mysqld &>>LOGFILE
+VALIDATE $? "Starting MY-SQL Server....."
+
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>LOGFILE
+VALIDATE $? "Setting up Root password..."
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
